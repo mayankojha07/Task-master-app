16d81ff19da98a1a92faf4cbaad5a0b5eddc5022
Mayank Ojha <ojhamayank70@gmail.com>
2024-10-13 14:08:40 +0530

    Ready to upload on github

---
+++ frontend/src/pages/Modal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div>
+      <div>
+        <h1>Hello </h1>
+      </div>
+    </div> // Codium
+  );
+};
+
+export default Modal;
